b7207cfb23df02c09750fd066e5b81003c579cfe
Prudhvi <prudhvi397@gmail.com>
2024-08-24 22:00:41 -0500

    Hyperparameter Training Completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -85,7 +85,9 @@ class ModelTrainer:
             }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models,param=params)
+                                             models=models,
+                                             param=params
+                                             )
             
             best_model_score = max(sorted(model_report.values()))
 
